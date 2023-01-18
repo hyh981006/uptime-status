418df38bb57c93e81d0509862b0b114985fdb606
爱吃大饼的小虎 <57065525+hyh981006@users.noreply.github.com>
2023-01-18 14:04:14 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,10 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u1878405-72dc3f345d3353ff36a0059e',
   ],
 
   // 日志天数
@@ -21,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '七鳄首页',
+      url: 'https://gmcj0816.top/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: '七鳄学习格',
+      url: 'https://vblog.gmcj0816.top'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: '七鳄学习格(vercel)',
+      url: 'https://vblog.gmcj0816.top/'
     },
   ],
 };
